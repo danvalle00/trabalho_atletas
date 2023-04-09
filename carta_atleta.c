29ff98589235fda7c291f3ee77b5276f6c799c27
Daniel do Valle <92067168+danvalle00@users.noreply.github.com>
2023-04-09 19:46:01 -0300

    ta terminando

--- carta_atleta.c
+++ carta_atleta.c
@@ -1,9 +1,36 @@
 #include "lib/function_atleta.h"
 
+// TODO: Fazer a função de ordenar a linkedlist.
+
 int main()
 {
+  node_t *head = NULL;
+
   carta_atleta_t *carta_atleta = malloc(TAM_deck * sizeof(carta_atleta_t));
-  popular_tabela(carta_atleta, TAM_deck);
+  carta_atleta_t *atleta_extras = malloc(EXTRA_deck * sizeof(carta_atleta_t));
+  carta_atleta_t *jogador_csv = malloc(TAM_deck * sizeof(carta_atleta_t));
+
+  gerar_jogador(carta_atleta, TAM_deck);
+  gerar_jogador_extra(atleta_extras, EXTRA_deck);
+
   salvar_tabela_csv(carta_atleta, TAM_deck, "lista_jogadores.csv");
+  extrair_jogador_csv("lista_jogadores.csv", jogador_csv, TAM_deck);
+
+  for (int i = 0; i < TAM_deck; i++)
+  {
+    add_node_final(&head, &jogador_csv[i]);
+  }
+  // TODO: Chamar a function de ordenar aqui
+  for (int i = 0; i < EXTRA_deck; i++)
+  {
+    add_node_final(&head, &atleta_extras[i]);
+  }
+
+  // TODO: Chamar a function de novo aqui
+  printar_node(head);
+
+  free(jogador_csv);
+  free(atleta_extras);
   free(carta_atleta);
+  free(head);
 }
--- lib/function_atleta.h
+++ lib/function_atleta.h
@@ -12,12 +12,18 @@
 #define TAM_name 20
 #define QTD_name 35
 #define QTD_time 20
-#define TAM_deck 100
+#define TAM_deck 1000
+#define EXTRA_deck 100
 #define range_random 100
 #define max_age 40
 #define min_age 16
 #define max_value 10000
 #define min_value 1000
+#define FILE_HEADER "ID,Nome,Sobrenome,Nome Completo,Idade,Time,Valor,Overall,Força,Resistencia,Velocidade,Força de vontade,Criatividade,Machucado,Posição,Liderança,Tempo De Recuperação\n"
+#define FILE_FORMAT "%d,%s,%s,%s,%d,%s,%d,%d,%d,%d,%d,%d,%d,%s,%s,%s,%d\n"
+#define NOMES_ATLETAS "Miguel", "Davi", "Gabriel", "Arthur", "Lucas", "Matheus", "Pedro", "Guilherme", "Gustavo", "Rafael", "Felipe", "Bernardo", "Enzo", "Nicolas", "João Pedro", "Cauã", "Vitor", "Eduardo", "Daniel", "Henrique", "Murilo", "Vinicius", "Samuel", "Pietro", "João Vitor", "Leonardo", "Caio", "Heitor", "Lorenzo", "Isaac", "Lucca", "Thiago", "João Gabriel", "João", "Marcos"
+#define SOBRENOMES_ATLETAS " Silva", " Santos", " Oliveira", " Souza", " Rodrigues", " Ferreira", " Alves", " Pereira", " Lima", " Gomes", " Costa", " Ribeiro", " Martins", " Carvalho", " Almeida", " Lopes", " Soares", " Fernandes", " Vieira", " Barbosa", " Rocha", " Dias", " Nascimento", " Andrade", " Moreira", " Nunes", " Marques", " Machado", " Mendes", " Freitas", " Cardoso", " Ramos", " Gonçalves", " Santana", " Teixeira"
+#define CLUBES "América-MG", "Athletico-PR", "Atlético-MG", "Bahia", "Botafogo", "Bragantino", "Corinthians", "Coritiba", "Cruzeiro", "Cuiabá", "Flamengo", "Fluminense", "Fortaleza", "Goiás", "Grêmio", "Internacional", "Palmeiras", "Santos", "São Paulo", "Vasco"
 
 typedef enum
 {
@@ -48,8 +54,30 @@ typedef struct
     int meses_recuperacao;
 } carta_atleta_t;
 
-void popular_tabela(carta_atleta_t *carta_atleta, int tamanho);
+typedef struct node
+{
+    carta_atleta_t *data_atleta;
+    struct node *proximo;
+} node_t;
+
+typedef struct jogador_csv
+{
+    int id;
+    char nome_completo[TAM_name * 2];
+} jogador_csv_t;
+
+void gerar_jogador(carta_atleta_t *carta_atleta, int tamanho);
+
+void gerar_jogador_extra(carta_atleta_t *carta_atleta, int tamanho);
+
 void salvar_tabela_csv(carta_atleta_t *carta_atleta, int tamanho, const char *nome_arquivo);
-int comparar_pessoas_por_nome(const void *p1, const void *p2);
+
+void add_node(node_t **head, carta_atleta_t *atleta);
+
+void add_node_final(node_t **head, carta_atleta_t *atleta);
+
+void printar_node(node_t *head);
+
+void extrair_jogador_csv(char *nome_arquivo, carta_atleta_t *jogador_csv, int tamanho);
 
 #endif
