8c02f0cdf5788742ccf5e453513bfe2605434390
Daniel do Valle <92067168+danvalle00@users.noreply.github.com>
2023-04-09 04:09:42 -0300

    extrair jogador do csv, define novos e coisitassss
    TODO: fazer todo o sort e colocar na linkedlist

--- carta_atleta.c
+++ carta_atleta.c
@@ -1,9 +1,23 @@
 #include "lib/function_atleta.h"
 
+// TODO: Implementar uma função de sort da lista encadeada, organizar a lista antes de gerar os 100 extras. Assim, gerar os 100 e manter ordenado.
+
 int main()
 {
+  node_t *head = NULL;
+
   carta_atleta_t *carta_atleta = malloc(TAM_deck * sizeof(carta_atleta_t));
-  popular_tabela(carta_atleta, TAM_deck);
+  carta_atleta_t *atleta_extras = malloc(EXTRA_deck * sizeof(carta_atleta_t));
+  jogador_csv_t *jogador_csv = malloc(TAM_deck * sizeof(jogador_csv_t));
+
+  gerar_jogador(carta_atleta, TAM_deck);
+  gerar_jogador_extra(atleta_extras, EXTRA_deck);
+
   salvar_tabela_csv(carta_atleta, TAM_deck, "lista_jogadores.csv");
+  extrair_jogador_csv("lista_jogadores.csv", jogador_csv, TAM_deck);
+
+  free(jogador_csv);
+  free(atleta_extras);
   free(carta_atleta);
+  free(head);
 }
--- lib/function_atleta.c
+++ lib/function_atleta.c
@@ -1,14 +1,12 @@
 #include "function_atleta.h"
 
-void popular_tabela(carta_atleta_t *carta_atleta, int tamanho)
+void gerar_jogador(carta_atleta_t *carta_atleta, int tamanho)
 {
   srand(time(NULL));
 
-  char nomes[QTD_name][TAM_name] = {"Miguel", "Davi", "Gabriel", "Arthur", "Lucas", "Matheus", "Pedro", "Guilherme", "Gustavo", "Rafael", "Felipe", "Bernardo", "Enzo", "Nicolas", "João Pedro", "Cauã", "Vitor", "Eduardo", "Daniel", "Henrique", "Murilo", "Vinicius", "Samuel", "Pietro", "João Vitor", "Leonardo", "Caio", "Heitor", "Lorenzo", "Isaac", "Lucca", "Thiago", "João Gabriel", "João", "Marcos"};
-
-  char sobrenomes[QTD_name][TAM_name] = {" Silva", " Santos", " Oliveira", " Souza", " Rodrigues", " Ferreira", " Alves", " Pereira", " Lima", " Gomes", " Costa", " Ribeiro", " Martins", " Carvalho", " Almeida", " Lopes", " Soares", " Fernandes", " Vieira", " Barbosa", " Rocha", " Dias", " Nascimento", " Andrade", " Moreira", " Nunes", " Marques", " Machado", " Mendes", " Freitas", " Cardoso", " Ramos", " Gonçalves", " Santana", " Teixeira"};
-
-  char times[QTD_time][TAM_name] = {"América-MG", "Athletico-PR", "Atlético-MG", "Bahia", "Botafogo", "Bragantino", "Corinthians", "Coritiba", "Cruzeiro", "Cuiabá", "Flamengo", "Fluminense", "Fortaleza", "Goiás", "Grêmio", "Internacional", "Palmeiras", "Santos", "São Paulo", "Vasco"};
+  char nomes[QTD_name][TAM_name] = {NOMES_ATLETAS};
+  char sobrenomes[QTD_name][TAM_name] = {SOBRENOMES_ATLETAS};
+  char times[QTD_time][TAM_name] = {CLUBES};
 
   for (int i = 0; i < tamanho; i++)
   {
@@ -49,22 +47,130 @@ void salvar_tabela_csv(carta_atleta_t *carta_atleta, int tamanho, const char *no
 
   FILE *arquivo = fopen(nome_arquivo, "w");
 
-  qsort(carta_atleta, tamanho, sizeof(carta_atleta_t), comparar_pessoas_por_nome);
-
   if (arquivo != NULL)
   {
-    fprintf(arquivo, "ID,Nome,Sobrenome,Nome Completo,Idade,Time,Valor,Overall,Força,Resistencia,Velocidade,Força de vontade,Criatividade,Machucado,Posição,Liderança,Tempo De Recuperação\n");
+    fprintf(arquivo, FILE_HEADER);
     for (int i = 0; i < tamanho; i++)
     {
-      fprintf(arquivo, "%d,%s,%s,%s,%d,%s,%d,%d,%d,%d,%d,%d,%d,%s,%s,%s,%d\n", carta_atleta[i].id, carta_atleta[i].nome, carta_atleta[i].sobrenome, carta_atleta[i].nome_completo, carta_atleta[i].idade, carta_atleta[i].time, carta_atleta[i].valor, (carta_atleta[i].forca + carta_atleta[i].forca_vontade + carta_atleta[i].velocidade + carta_atleta[i].criatividade + carta_atleta[i].resistencia) / 5, carta_atleta[i].forca, carta_atleta[i].resistencia, carta_atleta[i].velocidade, carta_atleta[i].forca_vontade, carta_atleta[i].criatividade, carta_atleta[i].machucado ? "Sim" : "Não", posicoes[carta_atleta[i].posicao], carta_atleta[i].lideranca ? "Sim" : "Não", carta_atleta[i].meses_recuperacao);
+      fprintf(arquivo, FILE_FORMAT, carta_atleta[i].id, carta_atleta[i].nome, carta_atleta[i].sobrenome, carta_atleta[i].nome_completo, carta_atleta[i].idade, carta_atleta[i].time, carta_atleta[i].valor, (carta_atleta[i].forca + carta_atleta[i].forca_vontade + carta_atleta[i].velocidade + carta_atleta[i].criatividade + carta_atleta[i].resistencia) / 5, carta_atleta[i].forca, carta_atleta[i].resistencia, carta_atleta[i].velocidade, carta_atleta[i].forca_vontade, carta_atleta[i].criatividade, carta_atleta[i].machucado ? "Sim" : "Não", posicoes[carta_atleta[i].posicao], carta_atleta[i].lideranca ? "Sim" : "Não", carta_atleta[i].meses_recuperacao);
     }
     fclose(arquivo);
   }
 }
 
-int comparar_pessoas_por_nome(const void *p1, const void *p2)
+node_t *criar_node(carta_atleta_t *atleta)
 {
-  const carta_atleta_t *a = p1;
-  const carta_atleta_t *b = p2;
-  return strcmp(a->nome_completo, b->nome_completo);
+  node_t *novo_node = malloc(sizeof(node_t));
+  novo_node->data_atleta = atleta;
+  novo_node->proximo = NULL;
+  return novo_node;
 }
+
+void add_node(node_t **head, carta_atleta_t *atleta)
+{
+  node_t *novo_node = criar_node(atleta);
+  novo_node->proximo = *head;
+  *head = novo_node;
+}
+
+void add_node_final(node_t **head, carta_atleta_t *atleta)
+{
+  node_t *novo_nodo = criar_node(atleta);
+  node_t *proximo = *head;
+  if (*head == NULL)
+  {
+    *head = novo_nodo;
+    return;
+  }
+  while (proximo->proximo != NULL)
+  {
+    proximo = proximo->proximo;
+  }
+  proximo->proximo = novo_nodo;
+}
+
+void printar_node(node_t *head)
+{
+  node_t *atual = head;
+  while (atual != NULL)
+  {
+    printf("%d, ", atual->data_atleta->id);
+    printf("%s\n ", atual->data_atleta->nome_completo);
+
+    atual = atual->proximo;
+  }
+}
+
+// --> Função feita pra tirar o conflito dos ID's dos jogadores novos com os antigos.
+void gerar_jogador_extra(carta_atleta_t *carta_atleta, int tamanho)
+{
+  srand(time(NULL));
+
+  char nomes[QTD_name][TAM_name] = {NOMES_ATLETAS};
+  char sobrenomes[QTD_name][TAM_name] = {SOBRENOMES_ATLETAS};
+  char times[QTD_time][TAM_name] = {CLUBES};
+
+  // --> O ID dos jogadores extras vão variar de 1000 - 1099
+  for (int i = 0; i < tamanho; i++)
+  {
+    int nome_index = rand() % QTD_name;
+    int sobrenome_index = rand() % QTD_name;
+    int time_index = rand() % QTD_time;
+
+    carta_atleta[i].id = (i + TAM_deck);
+    strcpy(carta_atleta[i].nome, nomes[nome_index]);
+    strcpy(carta_atleta[i].sobrenome, sobrenomes[sobrenome_index]);
+    strcpy(carta_atleta[i].nome_completo, nomes[nome_index]);
+    strcpy(carta_atleta[i].nome_completo, strcat(carta_atleta[i].nome_completo, carta_atleta[i].sobrenome));
+    carta_atleta[i].idade = (rand() % (max_age - min_age + 1)) + min_age;
+    strcpy(carta_atleta[i].time, times[time_index]);
+    carta_atleta[i].valor = (rand() % (max_value - min_value + 1)) + min_value;
+    carta_atleta[i].forca = rand() % range_random + 1;
+    carta_atleta[i].velocidade = rand() % range_random + 1;
+    carta_atleta[i].resistencia = rand() % range_random + 1;
+    carta_atleta[i].forca_vontade = rand() % range_random + 1;
+    carta_atleta[i].criatividade = rand() % range_random + 1;
+    carta_atleta[i].lideranca = rand() % 2;
+    carta_atleta[i].machucado = rand() % 2;
+    carta_atleta[i].posicao = rand() % 5;
+    if (carta_atleta[i].machucado)
+    {
+      carta_atleta[i].meses_recuperacao = 1 + rand() % 12;
+    }
+    else
+    {
+      carta_atleta[i].meses_recuperacao = 0;
+    }
+  }
+}
+
+void extrair_jogador_csv(char *nome_arquivo, jogador_csv_t *jogador_csv, int tamanho)
+{  
+  char linha[1024];
+  int j = 0;
+
+  FILE *arquivo = fopen(nome_arquivo, "r");
+  fgets(linha, sizeof(linha), arquivo); 
+  
+  while ((fgets(linha, sizeof(linha), arquivo)) != NULL) // le tds as linhas
+  {      
+    char *token = strtok(linha, ",");
+    jogador_csv[j].id = atoi(token);
+    token = strtok(NULL, ",");
+    for (int i = 0; i < 2; i++) // pula o nome e o sobrenome 
+    {
+      token = strtok(NULL, ",");
+    }
+    
+    strcpy(jogador_csv[j].nome_completo, token);
+    
+    for (int i = 0; i < 13; i++) // pulando o resto
+    {
+      token = strtok(NULL, ","); 
+    }
+    
+    j++;
+  }
+  fclose(arquivo);
+}
+          
